4626c239595aa16c31b75eee966478867fc70e0f
JayantHariharan <jayanthariharan@gmail.com>
2026-04-03 19:19:15 +0530

    chore(migrations): remove test migrations after verification
    These were created to test the migration system end-to-end.
    They are no longer needed and should not be part of the main schema.

--- flyway/sql/V3__drop_test_table.sql
+++
@@ -1,8 +0,0 @@
--- Migration V3: Drop the test table
--- Version: 3
--- Description: Drops the test_table created in V2
---
--- This migration removes the test_table after it has served its purpose
--- for testing the migration workflow.
-
-DROP TABLE IF EXISTS test_table{table_suffix};
